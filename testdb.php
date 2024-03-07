38f691957918c1c676b5f2b96d0d00535185a436
Matius54 <mat54200454@gmail.com>
2024-03-07 15:27:01 -0400

    cambios pequeños en testdb.php XD

--- testdb.php
+++ testdb.php
@@ -1,9 +1,9 @@
 <?php
     require_once "./db.php";
-    require_once "./utils.php";
+    //require_once "./utils.php";
     $cn = DB::getInstance();
-    //$cn->execute("INSERT INTO nomames (name, description) VALUES (?,?)",["hola","subnormal"]);
-    $cn->execute("SELECT * FROM nomames");
+    //$cn->execute("INSERT INTO tabla (name, description) VALUES (?,?)",["hola","xd"]);
+    $cn->execute("SELECT * FROM tabla");
     var_dump($cn->rowCount());
     $result = $cn->fetch();
     var_dump($result);
